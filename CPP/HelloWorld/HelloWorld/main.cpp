e3f947136cb3daa438d7b87302a566b48b00cc47
tebahjaffe <tebah.jaffe.pro@gmail.com>
2018-09-19 15:52:58 -0400

    new adds

---
+++ CPP/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Tebah saboun on 6/12/18.
+//  Copyright © 2018 Tebah saboun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    int i{2};
+    i=3.2;
+    i=2.9;
+    i=-1;
+    
+    unsigned int u{0};
+    u=-2;
+    
+    double d{2.7};
+    i=d;
+    d=i;
+    return 0;
+    
+}
